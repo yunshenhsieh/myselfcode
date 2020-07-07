48100bf0c003961db44c90fef4b51a7e1699f504
yunshenhsieh <67536792+yunshenhsieh@users.noreply.github.com>
2020-07-08 00:58:19 +0800

    Revert "modify content"
    This reverts commit f08246563e5053bddf70a51bd89dfc38b32d0339.

--- exceltocsv/exl_to_csv_pandas.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-data=pd.read_excel('C:\\Users\\user\\Desktop\\test.xls')
-b=data.to_csv(encoding='utf-8',index=0)
-b=b.split('\r\n')
-b='\n'.join(b[1:])
-with open('./test.csv','w',encoding='utf-8')as f:
-    f.write(b)
