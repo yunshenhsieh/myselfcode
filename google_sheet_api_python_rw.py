272055114514b27e48927e03d66a7eee7ff6ad7d
yunshenhsieh <67536792+yunshenhsieh@users.noreply.github.com>
2021-04-24 10:25:14 +0800

    version 1.0.1
    add timestamp, input while

--- google_sheet_api_python_rw.py
+++ google_sheet_api_python_rw.py
@@ -1,7 +1,9 @@
 # Teach video : https://www.youtube.com/watch?v=4ssigWmExak&list=PLxjXsyRHpX_hmj6048cCcMYvq5llD5cKO
+# Version 1.0.1
 # pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
+import datetime
 
 
 # If modifying these scopes, delete the file token.json.
@@ -26,11 +28,19 @@ result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
 print(result)
 
 # Write data into sheet
-n = len(result['values']) + 1
+drug_code_dict = {'955':'Ultracet'}
+# n = len(result['values']) + 1
 while True:
-    input_data = input("keyin data : ")
-    input_data = [data.strip() for data in input_data.split(',')]
-    SAMPLE_RANGE_NAME = "sheet1!A{}".format(n)
-    insert_data = [input_data]
-    request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME,
-                                valueInputOption="USER_ENTERED",body={"values":insert_data}).execute()
+    sheet_page_name = input("輸入藥品代碼 : ")
+    if sheet_page_name not in drug_code_dict:
+        print('此藥未登入，請重新輸入。')
+    else:
+        n = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                            range=drug_code_dict[sheet_page_name]).execute()
+        n = len(n['values']) + 1
+        input_data = input("{} 要拿取幾顆 : ".format(drug_code_dict[sheet_page_name]))
+        input_data = [datetime.datetime.now().strftime('%Y_%m_%d_%H_%M')] + [data.strip() for data in input_data.split(',')]
+        SAMPLE_RANGE_NAME = "{}!A{}".format(drug_code_dict[sheet_page_name],n)
+        insert_data = [input_data]
+        request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME,
+                                    valueInputOption="USER_ENTERED",body={"values":insert_data}).execute()
