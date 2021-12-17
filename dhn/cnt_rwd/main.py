88af630ac75a60ef1c34d4f862c87da9e33e6866
yunshenhsieh <67536792+yunshenhsieh@users.noreply.github.com>
2021-12-18 00:50:41 +0800

    version 1.0.1

--- dhn/cnt_rwd/main.py
+++ dhn/cnt_rwd/main.py
@@ -1,25 +1,34 @@
-from flask import Flask, send_file, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import json
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    ab =1234
-    cd ='456'
-    return render_template('rwd_cnt.html',**locals())
 
-@app.route('/a/cnt',methods=['GET'])
+    return render_template('index.html',**locals())
+
+@app.route('/cnt',methods=['GET'])
 def cnt():
+    userName = request.values['userName']
+    return render_template('rwd_cnt.html', **locals())
+
+@app.route('/cnt/?<string:userName>', methods=['GET'])
+def cntRe(userName):
+    userName = userName
+    return render_template('rwd_cnt.html', **locals())
+
+@app.route('/addData',methods=['GET'])
+def addData():
     v = request.values['returnData']
     print(v)
     print(type(v))
     v = json.loads(v)
     print(v)
     print(type(v))
-    free = request.values['multiSumFree']
-    print(json.loads(free))
     print('cnt')
-    return redirect('/')
+    print(type(v[0]))
+    userName = v[0]
+    return redirect(url_for('cntRe', userName=userName))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
