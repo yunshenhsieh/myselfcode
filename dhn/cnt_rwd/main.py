6bd4a66e5852d3811821b9d12faebce795c80a42
yunshenhsieh <67536792+yunshenhsieh@users.noreply.github.com>
2021-12-18 01:08:20 +0800

    version 1.0.2
    add try except.

--- dhn/cnt_rwd/main.py
+++ dhn/cnt_rwd/main.py
@@ -9,26 +9,35 @@ def index():
 
 @app.route('/cnt',methods=['GET'])
 def cnt():
-    userName = request.values['userName']
-    return render_template('rwd_cnt.html', **locals())
+    try:
+        userName = request.values['userName']
+        return render_template('rwd_cnt.html', **locals())
+    except:
+        return 'Error，請照指示使用。'
 
 @app.route('/cnt/?<string:userName>', methods=['GET'])
 def cntRe(userName):
-    userName = userName
-    return render_template('rwd_cnt.html', **locals())
+    try:
+        userName = userName
+        return render_template('rwd_cnt.html', **locals())
+    except:
+        return 'Error，請照指示使用。'
 
 @app.route('/addData',methods=['GET'])
 def addData():
-    v = request.values['returnData']
-    print(v)
-    print(type(v))
-    v = json.loads(v)
-    print(v)
-    print(type(v))
-    print('cnt')
-    print(type(v[0]))
-    userName = v[0]
-    return redirect(url_for('cntRe', userName=userName))
+    try:
+        v = request.values['returnData']
+        print(v)
+        print(type(v))
+        v = json.loads(v)
+        print(v)
+        print(type(v))
+        print('cnt')
+        print(type(v[0]))
+        userName = v[0]
+        return redirect(url_for('cntRe', userName=userName))
+    except:
+        return 'Error，請照指示使用。'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
