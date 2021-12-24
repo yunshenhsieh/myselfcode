a04ee5a20f7211d54abeaceed63fbf45d3fa96a7
yunshenhsieh <67536792+yunshenhsieh@users.noreply.github.com>
2021-12-24 23:07:10 +0800

    version 1.0.3
    add comment

--- dhn/cnt_rwd/main.py
+++ dhn/cnt_rwd/main.py
@@ -17,6 +17,7 @@ def cnt():
     except:
         return 'Error，請照指示使用。'
 
+# 讓使用者名稱可以直接帶入輸入網頁。
 @app.route('/cnt/?<string:userName>', methods=['GET'])
 def cntRe(userName):
     try:
@@ -25,6 +26,7 @@ def cntRe(userName):
     except:
         return 'Error，請照指示使用。'
 
+# 接收cnt網頁來的JS資料，轉送到google sheet。
 @app.route('/addData',methods=['GET'])
 def addData():
     global rowCnt
@@ -32,13 +34,14 @@ def addData():
         htmlJson = request.values['returnData']
         htmlJson = json.loads(htmlJson)
         userName = htmlJson[-1]
-        print(htmlJson)
         RwdGsheet.updateDataToGsheet(dataRearrange(htmlJson), 'rwd_demo', rowCnt)
+        # print(dataRearrange(htmlJson))
         rowCnt += 1
         return redirect(url_for('cntRe', userName=userName))
     except:
         return 'Error，請照指示使用。'
 
+# 整理網頁JS傳來的資料成想要的格式。
 def dataRearrange(data) -> list:
     result = [None for i in range(8)]
     result[0] = data[0]
@@ -50,16 +53,20 @@ def dataRearrange(data) -> list:
 
     return result
 
+# 整理自動乘倍數的資料，轉成想要的格式
 def dataInputRearrange(data) -> (str ,str):
     inputSum = data['sum']
     formula = ''
+    # 要字串轉成int值，排序後才都會由小到大，不然會出現1、28、7這種順序排列。
     dataKeys = sorted([int(i) for i in data.keys() if i != 'sum'])
+    # 轉出幾乘幾(x * x)的計算式出來。
     for k in dataKeys:
         formula += "{} * {} + ".format(k, data[str(k)])
     return (formula[:-3], inputSum)
 
 if __name__ == "__main__":
+    # 取得現在sheet的列數，這樣才不會把之前寫過的覆寫掉。
     rowCnt = len(RwdGsheet.getRowCnt('rwd_demo')['values']) + 1
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=80)
     # RwdGsheet.createGsheet('rwd')
     # print(len(RwdGsheet.getRowCnt('rwd_demo')['values']))
