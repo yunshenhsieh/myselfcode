e930f011238c7d52d27bcf811cab1b13e991c77a
yunshenhsieh <67536792+yunshenhsieh@users.noreply.github.com>
2022-04-27 16:29:31 +0800

    version 0.0.0
    while test version.

--- crawler/crawl_ece.py
+++ crawler/crawl_ece.py
@@ -1,66 +1,108 @@
 import ddddocr, time, random
 from bs4 import BeautifulSoup
+from datetime import datetime
 import selenium.webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
+import threading, ctypes, inspect
 
-def main():
-    headers = {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36",
-        "referer": "https://ap.ece.moe.edu.tw/webecems/pubSearch.aspx"
-        }
-    url = "https://ap.ece.moe.edu.tw/webecems/pubSearch.aspx"
-    seleniumOptions = selenium.webdriver.ChromeOptions()
-    seleniumOptions.add_argument("user-agent={}".format(headers["user-agent"]))
-    seleniumOptions.add_argument("referer={}".format(headers["referer"]))
-    seleniumOptions.add_experimental_option('excludeSwitches', ['enable-automation'])
-    driverExe = selenium.webdriver.Chrome(chrome_options=seleniumOptions, executable_path="../../driver/chromedriver.exe")
-
-    driverExe.get(url)
-    WebDriverWait(driverExe, 50).until(expected_conditions.presence_of_element_located((By.ID, "btnSearch")))
-    driverExe.find_element(By.ID, "btnSearch").click()
+def main(driverExe):
+    global recordPageNum, timeoutPoint, totalPage
+    print("main work")
+    time.sleep(10)
     WebDriverWait(driverExe, 50).until(expected_conditions.presence_of_element_located((By.ID, "divResult")))
     totalPage = int(BeautifulSoup(driverExe.page_source, "html.parser").select("span#lblRowCnt")[0].text.replace(",",""))
+    pageNum = int(BeautifulSoup(driverExe.page_source, "html.parser").select("span#PageControl1_lblCurrentPage")[0].text.replace(",",""))
+    recordPageNum = pageNum
+    failPageCnt = 0
+    while totalPage > pageNum:
+        if failPageCnt == 0:
+            time.sleep(10)
+            js="var action=document.documentElement.scrollTop=0"
+            driverExe.execute_script(js)
+            print("正在第 {} 頁。".format(pageNum))
+            soup = BeautifulSoup(driverExe.page_source, "html.parser")
+            itemNum = str(soup.select("table#GridView1")[0]).count("GridView1_btnMore")
+            itemCnt = 0
+            failItemCnt = 0
+            while itemNum > itemCnt:
+                try:
+                    itemName = soup.select("span#GridView1_lblSchName_{}".format(itemCnt))[0].text
+                    print("Download No.{} page, No.{} item, {}。".format(pageNum, itemCnt + 1, itemName))
+                    mainPageToDataPage(driverExe, itemCnt)
+                    timeoutPoint = False
+                    itemCnt += 1
+                except Exception as e:
+                    if failItemCnt < 4 :
+                        failItemCnt += 1
+                        driverSwitchLastWindow(driverExe)
+                        time.sleep(10)
+                    else:
+                        with open("./error_log.txt", "a", encoding="utf-8")as w:
+                            errLog = "***{} | {} | 第 {} 頁 | 第 {} 項 | 原因：{}\n".format(datetime.now(), itemName, pageNum, itemCnt + 1, e)
+                            w.write(errLog)
+                        print(e)
+                        failItemCnt = 0
+                        itemCnt += 1
+                        driverSwitchLastWindow(driverExe)
 
-    for pageNum in range(totalPage):
-        print("正在第 {} 頁。".format(pageNum + 1))
-        itemNum = str(BeautifulSoup(driverExe.page_source, "html.parser").select("table#GridView1")[0]).count("GridView1_btnMore")
-        for num in range(itemNum):
-            print("Download No.{} item。".format(num + 1))
-            mainPageToDataPage(driverExe, num)
-        nextPage(driverExe)
+                        if "timeout" in e:
+                            global globalException
+                            globalException = e
+                        pass
+                timeoutPoint = False
+            try:
+                driverSwitchLastWindow(driverExe)
+                nextPage(driverExe)
+                failPageCnt = 0
+                timeoutPoint = False
+            except Exception as e:
+                failPageCnt += 1
+                with open("./error_log.txt", "a", encoding="utf-8") as w:
+                    errLog = "**{} | {} | 第 {} 頁 | 原因：{}\n".format(datetime.now(), itemName, pageNum, e)
+                    w.write(errLog)
+                driverSwitchLastWindow(driverExe)
+            timeoutPoint = False
+        else:
+            time.sleep(10)
+            driverSwitchLastWindow(driverExe)
+            nextPage(driverExe)
+            timeoutPoint = False
     pass
 
 def mainPageToDataPage(driverExe, num):
-    time.sleep(random.randint(4, 6))
-    driverExe.find_element(By.ID, "GridView1_btnMore_{}".format(num)).click()
+    time.sleep(random.randint(6, 9))
+    checkExtend = BeautifulSoup(driverExe.page_source, "html.parser").select("input#GridView1_btnMore_{}".format(num))[0]["value"]
+    if not ("收合" in checkExtend):
+        driverExe.find_element(By.ID, "GridView1_btnMore_{}".format(num)).click()
 
     time.sleep(5)
     validValue = getValidNum(driverExe, num)
 
     time.sleep(5)
+    driverExe.find_element(By.ID, "GridView1_txtVerify_{}".format(num)).clear()
     driverExe.find_element(By.ID, "GridView1_txtVerify_{}".format(num)).send_keys(validValue)
     time.sleep(5)
-    try:
-        driverExe.find_element(By.ID, "GridView1_lbChgList_{}".format(num)).click()
-        time.sleep(5)
-        driverSwitchLastWindow(driverExe)
-        getData(driverExe)
-        time.sleep(5)
-        driverExe.close()
-        driverSwitchLastWindow(driverExe)
-    except:
-        print("第 {} 項 失敗。".format(num))
+    driverExe.find_element(By.ID, "GridView1_lbChgList_{}".format(num)).click()
+    time.sleep(5)
+    driverSwitchLastWindow(driverExe)
+    getData(driverExe)
+    time.sleep(5)
+    driverExe.close()
+    driverSwitchLastWindow(driverExe)
     pass
 
 def getData(driverExe):
     soup = BeautifulSoup(driverExe.page_source, "html.parser")
     print(soup.select("span#lblSchName102")[0].text)
+    with open("./ece_result.txt", "a", encoding="utf-8")as w:
+        w.write(str(soup) + "\n")
     return
 
 def getValidNum(driverExe, num) -> str:
     renewValidNum(driverExe, num)
+    WebDriverWait(driverExe, 50).until(expected_conditions.presence_of_element_located((By.ID, "GridView1_imgValidateCode_{}".format(num))))
     time.sleep(random.randint(4, 6))
     driverExe.find_element(By.ID, "GridView1_imgValidateCode_{}".format(num)).screenshot("demo.jpg")
     global ocr
@@ -74,6 +116,9 @@ def getValidNum(driverExe, num) -> str:
             return validValue
         else:
             renewValidNum(driverExe, num)
+            WebDriverWait(driverExe, 50).until(
+                expected_conditions.presence_of_element_located((By.ID, "GridView1_imgValidateCode_{}".format(num))))
+            time.sleep(random.randint(4, 6))
             driverExe.find_element(By.ID, "GridView1_imgValidateCode_{}".format(num)).screenshot("demo.jpg")
             with open("./demo.jpg", "rb") as f:
                 tmp = f.read()
@@ -86,15 +131,101 @@ def renewValidNum(driverExe, num):
     pass
 
 def nextPage(driverExe):
-    driverExe.find_element(By.ID, "PageControl1_lbNextPage").click()
     time.sleep(random.randint(4, 6))
+    driverExe.switch_to.window(driverExe.window_handles[-1])
+    driverExe.find_element(By.ID, "PageControl1_lbNextPage").click()
     pass
+
+def switchToRecordPageNum(recordPageNum) -> object:
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36",
+        "referer": "https://ap.ece.moe.edu.tw/webecems/pubSearch.aspx"
+    }
+    url = "https://ap.ece.moe.edu.tw/webecems/pubSearch.aspx"
+    seleniumOptions = selenium.webdriver.ChromeOptions()
+    seleniumOptions.add_argument("user-agent={}".format(headers["user-agent"]))
+    seleniumOptions.add_argument("referer={}".format(headers["referer"]))
+    seleniumOptions.add_experimental_option('excludeSwitches', ['enable-automation'])
+    driverExe = selenium.webdriver.Chrome(chrome_options=seleniumOptions,
+                                          executable_path="../../driver/chromedriver.exe")
+
+    driverExe.get(url)
+    time.sleep(4)
+    WebDriverWait(driverExe, 50).until(expected_conditions.presence_of_element_located((By.ID, "btnSearch")))
+    driverExe.find_element(By.ID, "btnSearch").click()
+    time.sleep(5)
+    WebDriverWait(driverExe, 50).until(expected_conditions.presence_of_element_located((By.ID, "PageControl1_txtPages")))
+    driverExe.find_element(By.ID, "PageControl1_txtPages").clear()
+    driverExe.find_element(By.ID, "PageControl1_txtPages").send_keys(recordPageNum)
+    driverExe.find_element(By.ID, "PageControl1_lbPageChg").click()
+
+    return driverExe
 
 def driverSwitchLastWindow(driverExe):
     driverExe.switch_to.window(driverExe.window_handles[-1])
     pass
 
+def _async_raise(tid, exctype):
+    """raises the exception, performs cleanup if needed"""
+    try:
+        tid = ctypes.c_long(tid)
+        if not inspect.isclass(exctype):
+            exctype = type(exctype)
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
+        if res == 0:
+            # pass
+            raise ValueError("invalid thread id")
+        elif res != 1:
+            # """if it returns a number greater than one, you're in trouble,
+            # and you should call it again with exc=NULL to revert the effect"""
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, None)
+            raise SystemError("PyThreadState_SetAsyncExc failed")
+    except Exception as err:
+        print(err)
+
+def stop_thread(thread):
+    """终止线程"""
+    _async_raise(thread.ident, SystemExit)
+    print("stop thread")
+
 if __name__ == "__main__":
-    input("enter鍵開始")
+    recordPageNum = 1
+    recordItemNum = 0
+    timeoutNum = 0
+    globalException = ""
+    timeoutPoint = True
+    totalPage = 0
+
     ocr = ddddocr.DdddOcr()
-    main()
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36",
+        "referer": "https://ap.ece.moe.edu.tw/webecems/pubSearch.aspx"
+    }
+    url = "https://ap.ece.moe.edu.tw/webecems/pubSearch.aspx"
+    seleniumOptions = selenium.webdriver.ChromeOptions()
+    seleniumOptions.add_argument("user-agent={}".format(headers["user-agent"]))
+    seleniumOptions.add_argument("referer={}".format(headers["referer"]))
+    seleniumOptions.add_experimental_option('excludeSwitches', ['enable-automation'])
+    driverExe = selenium.webdriver.Chrome(chrome_options=seleniumOptions,
+                                          executable_path="../../driver/chromedriver.exe")
+    driverExe.get(url)
+    time.sleep(4)
+    WebDriverWait(driverExe, 50).until(expected_conditions.presence_of_element_located((By.ID, "btnSearch")))
+    driverExe.find_element(By.ID, "btnSearch").click()
+    time.sleep(5)
+    totalPage = int(BeautifulSoup(driverExe.page_source, "html.parser").select("span#lblRowCnt")[0].text.replace(",", ""))
+    print(totalPage)
+    driverExe.quit()
+
+    while recordPageNum < totalPage:
+        if timeoutPoint:
+            try:
+                stop_thread(thread1)
+            except:
+                print("error")
+            thread1 = threading.Thread(target=main, args=(switchToRecordPageNum(recordPageNum),))
+            thread1.start()
+            timeoutPoint = False
+        else:
+            timeoutPoint = True
+        time.sleep(300)
