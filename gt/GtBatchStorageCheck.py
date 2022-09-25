ef45ec65c2d04ebaf2a4533c13040f6125a4aaed
yunshenhsieh <67536792+yunshenhsieh@users.noreply.github.com>
2022-09-25 21:46:54 +0800

    version 1.1.1
    Add isAlertPresent function that is taking products have not post on website.

--- gt/GtBatchStorageCheck.py
+++ gt/GtBatchStorageCheck.py
@@ -2,13 +2,14 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import time, random
 import selenium.webdriver
+from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 
 def setSeleniumOptionsAndDriverExe() -> object:
     headers = {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
     }
     seleniumOptions = selenium.webdriver.ChromeOptions()
     seleniumOptions.add_argument("user-agent={}".format(headers["user-agent"]))
@@ -18,31 +19,45 @@ def setSeleniumOptionsAndDriverExe() -> object:
                                           executable_path="./driver/chromedriver.exe")
     return driverExe
 
+def isAlertPersent(driverExe) -> bool:
+    try:
+        alert = driverExe.switch_to.alert
+        alert.accept()
+        return True
+    except NoAlertPresentException as e:
+        return False
+
 def gtStorageCheck(driverExe, prodNumList, fileWriter0, fileWriter1):
     urlList = \
         ["https://shop.greattree.com.tw/product?SaleID={}".format(prodNum) for prodNum in prodNumList]
 
     for n, url in enumerate(urlList):
         driverExe.get(url)
-        WebDriverWait(driverExe, 50).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "pdcnt_info_number")))
-        time.sleep(5)
-        soup = BeautifulSoup(driverExe.page_source, "html.parser")
-        soup = soup.select("div.pdcnt_info_number")
-        prodNum = soup[0].select("option")[-1].text
-        if int(prodNum) == 0:
-            fileWriter0.write(prodNumList[n].strip() + "\n")
-            fileWriter0.flush()
-            print("商品編：{}，無庫存。".format(prodNumList[n].strip(), prodNum))
+        if not isAlertPersent(driverExe):
+            WebDriverWait(driverExe, 50).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "pdcnt_info_number")))
+            time.sleep(5)
+            soup = BeautifulSoup(driverExe.page_source, "html.parser")
+            soup = soup.select("div.pdcnt_info_number")
+            prodNum = soup[0].select("option")[-1].text
+            if int(prodNum) == 0:
+                fileWriter0.write(prodNumList[n].strip() + "\n")
+                fileWriter0.flush()
+                print("商品編：{}，無庫存。".format(prodNumList[n].strip()))
+            else:
+                fileWriter1.write("{}_{}\n".format(prodNumList[n].strip(), prodNum))
+                fileWriter1.flush()
+                print("商品編：{}，庫存為{}。".format(prodNumList[n].strip(), prodNum))
         else:
-            fileWriter1.write("{}_{}\n".format(prodNumList[n].strip(), prodNum))
-            fileWriter1.flush()
-            print("商品編：{}，庫存為{}。".format(prodNumList[n].strip(), prodNum))
+            fileWriter0.write(prodNumList[n].strip() + "，沒有上架。\n")
+            fileWriter0.flush()
+            print("商品編：{}，沒有上架。".format(prodNumList[n].strip()))
+
         time.sleep(random.randint(5, 7))
 
     pass
 
 if __name__ == "__main__":
-    print("版本：1.0.1\n發佈日期：2022/9/25\n作者：Vincent 燊。\n啟動執行中…")
+    print("版本：1.1.1\n發佈日期：2022/9/25\n作者：Vincent 燊。\n啟動執行中…")
     try:
         driverExe = setSeleniumOptionsAndDriverExe()
     except selenium.common.exceptions.SessionNotCreatedException as e:
