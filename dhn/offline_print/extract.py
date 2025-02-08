763e3e8547c88dd87802a30139cbfa4457833cdf
yunshenhsieh <67536792+yunshenhsieh@users.noreply.github.com>
2025-02-08 15:29:29 +0800

    Version 4.0.6
    In order to fix the bug when seperate header and medison information, add seperateHeaderAndMedicineInfo method.
    Make the var name to same.

--- dhn/offline_print/extract.py
+++ dhn/offline_print/extract.py
@@ -1,58 +1,64 @@
-# Version 4.0.5
-def extractReceiveNumber(contentForHeader: list[str]) -> str:
-    receiveNumber = contentForHeader[3].split('：')[-1][:5].strip()
+# Version 4.0.6
+def extractReceiveNumber(contentHeaderList: list[str]) -> str:
+    receiveNumber = contentHeaderList[3].split('：')[-1][:5].strip()
     return receiveNumber
 
-def extractChartNumber(contentForHeader: list[str]) -> str:
-    chartNumber = ''.join([n for n in contentForHeader[4][:9] if ord(n) != 32]).strip()
+def extractChartNumber(contentHeaderList: list[str]) -> str:
+    chartNumber = ''.join([n for n in contentHeaderList[4][:9] if ord(n) != 32]).strip()
     return chartNumber
 
-def extractPtName(contentForHeader: list[str]) -> str:
-    ptName = contentForHeader[5][:20].strip()
+def extractPtName(contentHeaderList: list[str]) -> str:
+    ptName = contentHeaderList[5][:20].strip()
     if '出' in ptName:
         ptName = ptName[:ptName.index('出')].strip()
     return ptName
 
-def extractBirthDay(contentForHeader: list[str]) -> str:
-    birthDay = ''.join([date for date in contentForHeader[5].split('年月日:')[-1][:11] if ord(date) != 32]).strip()
+def extractBirthDay(contentHeaderList: list[str]) -> str:
+    birthDay = ''.join([date for date in contentHeaderList[5].split('年月日:')[-1][:11] if ord(date) != 32]).strip()
     return birthDay
 
-def extractDepartment(contentForHeader: list[str]) -> str:
-    department = '急診' + contentForHeader[5].strip().split('急診')[-1]
+def extractDepartment(contentHeaderList: list[str]) -> str:
+    department = '急診' + contentHeaderList[5].strip().split('急診')[-1]
     return department
 
-def extractDoctorName(contentForHeader: list[str]) -> str:
-    doctorName = contentForHeader[5].split('醫師')[-2][-4:].strip()
+def extractDoctorName(contentHeaderList: list[str]) -> str:
+    doctorName = contentHeaderList[5].split('醫師')[-2][-4:].strip()
     return doctorName
 
-def sortSerialNumber(contentForDrugInfo: str) -> list:
-    # list[0]是開方醫師分隔完，剩下的一些部份，所以從list[1]開始才是藥品明細。
-    medisonList = [medisonData for medisonData in contentForDrugInfo.split('\n')[1:]]
-    # 因list[1]有可能是\n造成空值，所以合併為字串，用strip()消掉再split，這樣就可以解決這個可能的bug。
-    medisonList = '\n'.join(medisonList).strip()
-    medisonList = [medisonData.split('\t') for medisonData in medisonList.split('\n')]
+def sortSerialNumber(contentMedicineList: list) -> list:
+    medicineList = [medicineData.strip().split('\t') for medicineData in contentMedicineList]
 
-    for n, medisonData in enumerate(medisonList):
-        medisonList[n][0] = int(medisonData[0])
+    for n, medicineData in enumerate(medicineList):
+        medicineList[n][0] = int(medicineData[0])
 
-    medisonList = sorted(medisonList, key=lambda x: x[0])
+    medicineList = sorted(medicineList, key=lambda x: x[0])
 
-    for n, medisonData in enumerate(medisonList):
-        medisonList[n][0] = str(medisonData[0])
+    for n, medicineData in enumerate(medicineList):
+        medicineList[n][0] = str(medicineData[0])
 
-    return medisonList
+    return medicineList
 
-def extractMedisonInfo(contentForDrugInfo: str, drugProfileDict: dict) -> list:
+def extractMedicineInfo(contentMedicineList: list, drugProfileDict: dict) -> list:
     # 讓藥品按序號排列，發現藥袋列印順序也是看序號。
-    medisonList = sortSerialNumber(contentForDrugInfo)
+    medicineList = sortSerialNumber(contentMedicineList)
 
-    for n, medison in enumerate(medisonList):
-        drugId = medison[17].split(' ')[0].strip()
+    for n, medicine in enumerate(medicineList):
+        drugId = medicine[17].split(' ')[0].strip()
         # 藥品編號查無資料時，給13個空值的串列，這樣就不會因為串列長度不足而跳錯。
         drugCode = drugProfileDict.get(drugId, ['' for n in range(13)])[12]
-        medisonList[n][2] = drugId
-        medisonList[n][1] = drugCode
+        medicineList[n][2] = drugId
+        medicineList[n][1] = drugCode
         # 補足最後一行list長度，不然最後一行沒有備註的話，最後一格就會是藥品編號，列印時就會印在備註。
-        if len(medisonList[n]) < 20:
-            medisonList[n] = medisonList[n] + ([''] * (20 - len(medisonList[n])))
-    return medisonList
+        if len(medicineList[n]) < 20:
+            medicineList[n] = medicineList[n] + ([''] * (20 - len(medicineList[n])))
+    return medicineList
+
+def seperateHeaderAndMedicineInfo(contentList: list) -> list:
+    contentHeaderList = []
+    contentMedicineList = []
+    for content in contentList:
+        if '\t' in content:
+            contentMedicineList.append(content)
+        else:
+            contentHeaderList.append(content)
+    return contentHeaderList, contentMedicineList
